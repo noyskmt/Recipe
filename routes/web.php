3807926a6cfa2035260d51634f7997d986aed342
naoya sakamoto <n.sakamoto.sy@gmail.com>
2023-05-23 19:49:11 +0900

    inertiaをインストール

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,35 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\UserController;
-use App\Http\Controllers\RecipeSrchController;
-// use Illuminate\Foundation\Application;
-// use Inertia\Inertia;
+use Inertia\Inertia;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/toppage',[RecipeSrchController::class, 'index']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
