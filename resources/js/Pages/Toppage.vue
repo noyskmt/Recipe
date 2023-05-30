4ea615475b2cd0308e233a51461cce5080842d75
FumiyaAdachi <adachi@launchcraft.jp>
2023-05-30 20:01:21 +0900

    scss周りのファイルを追加

--- resources/js/Pages/Toppage.vue
+++ resources/js/Pages/Toppage.vue
@@ -1,7 +1,7 @@
-<template>
-    <Slide>
-        <a><span>Home1</span></a>
-        <a><span>Home2</span></a>
+<template class="parent">
+    <Slide class="child">
+        <a><span class="hoge">Home1</span></a>
+        <a><span class="moge">Home2</span></a>
         <a><span>Home3</span></a>
         <a><span>Home4</span></a>
     </Slide>
@@ -9,7 +9,6 @@
 
 <script>
 import { Slide } from 'vue3-burger-menu';
-
 export default {
     components: {
         Slide
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-import './bootstrap';
-import '../css/app.css';
 import '../sass/app.scss';
 
 import { createApp, h } from 'vue';
