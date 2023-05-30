47cc4f52b14ae2af1b5f1de15fc139d9cd91a34c
FumiyaAdachi <adachi@launchcraft.jp>
2023-05-30 18:45:50 +0900

    項目を追加

--- resources/js/Pages/Toppage.vue
+++ resources/js/Pages/Toppage.vue
@@ -1,8 +1,9 @@
 <template>
     <Slide>
-        <a id="home" href="#">
-            <span>Home</span>
-        </a>
+        <a><span>Home1</span></a>
+        <a><span>Home2</span></a>
+        <a><span>Home3</span></a>
+        <a><span>Home4</span></a>
     </Slide>
 </template>
 
