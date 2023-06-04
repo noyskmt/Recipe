393bb9fa04e5751de905812f26b7061c7ded4e60
naoya sakamoto <n.sakamoto.sy@gmail.com>
2023-06-04 19:24:47 +0900

    コーディング ・toppageフロント部完了

--- resources/js/Pages/Sidebar.vue
+++ resources/js/Pages/Sidebar.vue
@@ -3,18 +3,25 @@
         <div class="sidebar-wrapper">
             <div class="sidebar-switching">
                 <button class="btn btn-outline-dark">食材</button>
-                <button class="btn btn-outline-dark" style="margin-left: 50px;">調味料</button>
+                <button class="btn btn-outline-dark" style="margin-left: 20%;">調味料</button>
             </div>
             <div class="add">
-                <input type="text" v-model="add" class="form-control" style="max-width:200px;">
-                <button class="btn btn-dark" style="margin-left: 10px;">追加</button>
+                <input type="text" class="form-control">
+                <button class="btn btn-dark">追加</button>
             </div>
             <div class="added-content">
                 <div class="form-check form-switch">
                     <input class="form-check-input" type="checkbox" id="switch_1" name="switch_1" checked>
                     <label class="form-check-label" for="switch_1">食材名ループ処理</label>
                 </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="switch_1" name="switch_1" checked>
+                    <label class="form-check-label" for="switch_1">食材名</label>
+                </div>
             </div>
+        </div>
+        <div class="store-link">
+            <button class="btn btn-light">店リスト</button>
         </div>
     </div>
 </template>
@@ -23,35 +30,3 @@
     // import { Inertia } from '@inertiajs/inertia';
 
 </script>
-
-<style>
-    .sidebar {
-        background-color: #19197043;
-        /* opacity: 0.9.5; 透過する 0に近くほど透過する */
-        position: fixed; /*要素を固定する(スクロールしても位置は固定される) */
-        left: 1040px;
-        height: 100%; /*サイドバーの高さ*/
-        width: 400px; /* サイドバーのwidthを指定 */
-        max-width: 400px; /* widthの最大値 */
-        /* overflow-x: hidden; 横軸ではみ出た要素を非表示にする */
-        box-sizing: border-box; /* paddingとborderを、widthとheightに含める */
-        padding-left: 40px; /* サイドバー内のリンクの位置を右にずらす */
-    }
-
-    .sidebar-switching {
-        margin: 30px 70px; 
-    }
-
-    .add {
-        display: flex;
-        justify-content: center;
-        padding-bottom: 30px;
-    }
-
-    .added-content{
-        display: flex;
-        justify-content: center;
-    }
-
-
-</style>
--- resources/js/Pages/Toppage.vue
+++ resources/js/Pages/Toppage.vue
@@ -1,35 +1,33 @@
 <template>
-    <div>
-        <p>window width: {{ width }}</p>
-        <p>window height: {{ height }}</p>
-    </div>
-    <div>
-        <sidebar class="sidebar-area"></sidebar>
-        <div class="toppage-wrapper">
-            <router-view/>
-        </div>
-    </div>
-    <div class="toppage-wrapper">
-        <div class="menu">
-            <button class="btn btn-outline-dark">履歴</button>
-            <button class="btn btn-outline-dark" style="margin-left: 10px;">モード</button>
-            <button class="btn btn-dark" style="margin-left: 50px;">検索</button>
-        </div>
-        <div class="main">
-            <h1>レシピサーチ</h1>
-        
-            <div class="favoriteRecipe">
-                <!-- ループ処理 -->
-                <tr>お気に入りしたレシピ</tr> 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8">
+                <div class="menu">
+                    <button class="btn btn-outline-dark">履歴</button>
+                    <button class="btn btn-outline-dark" style="margin-left: 10px;">モード</button>
+                    <button class="btn btn-dark" style="margin-left: 50px;">検索</button>
+                </div>
+                <div class="main">
+                    <h1>レシピサーチ</h1>
+                
+                    <div class="favoriteRecipe">
+                        <!-- ループ処理 -->
+                        <tr>お気に入りしたレシピ</tr> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <sidebar class="sidebar-area"></sidebar>
+                <div class="toppage-wrapper">
+                    <router-view/>
+                </div>
             </div>
         </div>
-    </div>
+    </div>    
 </template>
 
 <script>
     // import { Inertia } from '@inertiajs/inertia';
-    // import 'bootstrap/dist/css/bootstrap.css';
-    // import 'bootstrap-vue/dist/bootstrap-vue.css';
     import Sidebar from './Sidebar.vue';
 
     export default{
@@ -37,53 +35,6 @@
             Sidebar
         },
 
-        data: function () {
-            return {
-                width: window.innerWidth,
-                height: window.innerHeight
-            }
-        },
-
-        methods: {
-            handleResize: function() {
-                this.width = window.innerWidth,
-                this.height = window.innerHeight
-            }
-        },
-
-        mounted: function() {
-            window.addEventListener('resize', this.handleResize)
-        },
-
-        beforeDestroy: function () {
-            window.removeEventListener('resize', this.handleResize)
-        }
     }
 
 </script>
-
-<style>
-    /* .toppage-wrapper { */
-        /* float: left; */
-
-        /* サイドバーのwidth分だけ範囲を削除 */
-        /* width: calc(100% - 700px); */
-
-        /* サイドバーで隠れるので右に寄せる */
-        /* margin: 0 0 0 180px; */
-    /* } */
-
-    .menu {
-        padding: 30px 30px 30px 730px;
-    }
-
-    .main {
-        width: 100%;
-        margin: 50px 410px;
-    }
-
-    .favoriteRecipe {
-        margin: 20px 40px;
-    }
-
-</style>
