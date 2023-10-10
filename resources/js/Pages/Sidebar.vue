9cca8d877251838274328528391ea6fffe314da4
naoya sakamoto <n.sakamoto.sy@gmail.com>
2023-10-10 18:58:05 +0900

    フォーム入力時にカテゴリー(0,1)をつける実装の途中です

--- resources/js/Pages/Sidebar.vue
+++ resources/js/Pages/Sidebar.vue
@@ -5,27 +5,29 @@
                 <button @click="clickTab(true)" class="btn" :class="{ 'btn-dark b': isTabFlag, 'btn-dark a': !isTabFlag }">食材</button>
                 <button @click="clickTab(false)" class="btn" :class="{ 'btn-dark a': isTabFlag, 'btn-dark b': !isTabFlag }">調味料</button>
             </div>
-            <div class="add">
-                <input type="text" v-model="newFood" class="form-control">
-                <button @click="addFood()" class="btn btn-dark">追加</button>
-            </div>
-            <div class="added-content">
-                <div v-for="food in foods">
-                    <div v-if="food.categories == '0'">
-                        <div v-show="isTabFlag"  key="ingredient">
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" id="switch_1" name="switch_1" checked>
-                                <label @click="openModal" class="form-check-label">{{ food.name }}</label>
-                                <MyModal @close="closeModal" v-if="modal"></MyModal>
+            <div class="food-list">
+                <div class="add">
+                    <input type="text" v-model="newFood" class="form-control">
+                    <button @click="addFood()" class="btn btn-dark">追加</button>
+                </div>
+                <div class="added-content">
+                    <div v-for="food in foods">
+                        <div v-if="food.categories == '0'">
+                            <div v-show="isTabFlag"  key="ingredient">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="switch_1" name="switch_1" checked>
+                                    <label @click="openModal" class="form-check-label">{{ food.name }}</label>
+                                    <MyModal @close="closeModal" v-if="modal"></MyModal>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div v-else>
-                        <div v-show="!isTabFlag" key="seasoning">
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" id="switch_1" name="switch_1" checked>
-                                <label @click="openModal" class="form-check-label">{{ food.name }}</label>
-                                <MyModal @close="closeModal" v-if="modal"></MyModal>
+                        <div v-else>
+                            <div v-show="!isTabFlag" key="seasoning">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="switch_1" name="switch_1" checked>
+                                    <label @click="openModal" class="form-check-label">{{ food.name }}</label>
+                                    <MyModal @close="closeModal" v-if="modal"></MyModal>
+                                </div>
                             </div>
                         </div>
                     </div>
